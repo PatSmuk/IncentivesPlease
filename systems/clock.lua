882e6b31d8f45a4b6e9a1ca246e6015c0882ff97
Peter Little <Plittle@360incentives.com>
2016-07-18 14:58:55 -0400

    fixed clock bugs and added AM and PM functionality

--- systems/clock.lua
+++ systems/clock.lua
@@ -3,7 +3,7 @@ local DAY_END = messages.DAY_END
 local START_HOUR = 9
 local START_MIN = 0
 local END_HOUR = 5
-local END_MIN = 0
+local END_MIN = 59
 
 function registerClock(game)
   print("Registering clock system")
@@ -11,15 +11,17 @@ function registerClock(game)
   game.clock.currentHour = START_HOUR
   game.clock.currentMin = START_MIN
   game.clock.clockRunning = true
+  game.clock.amOrPm = 'AM';
   
   game:on('DAY_START', resetClock)
   game:on('UPDATE', updateClock)
-  game:on('RENDER_UI', renderClock)
+  game:on('DAY_START', renderClock)
 end
 
 function resetClock(game, message)
 	game.clock.currentHour = START_HOUR
 	game.clock.currentMin = START_MIN
+	game.clock.amOrPm = 'AM';
 end
 
 function updateClock(game, message)
@@ -27,25 +29,30 @@ function updateClock(game, message)
 		return
 	end
 
-	game.clock.currentMin = game.clock.currentMin + (message.dt * 3)
+	game.clock.currentMin = game.clock.currentMin + (message.dt * 15)
 
-	if game.clock.currentMin > 60 then
+	if game.clock.currentMin >= END_MIN then
 		game.clock.currentHour = game.clock.currentHour + 1
 		game.clock.currentMin = 0
 	end
 
-	if game.clock.currentHour > 13 then
+	if game.clock.currentHour >= 12 then
+		game.clock.amOrPm = 'PM'
+	end
+
+	if game.clock.currentHour > 12 then
 		game.clock.currentHour = 1
 	end
 
 	if game.clock.currentHour == END_HOUR then
 		game:dispatch(DAY_END())
-		clockRunning = false
+		game.clock.clockRunning = false
+		return
 	end
 end
 
 function renderClock(game, message)
-	love.graphics.print(string.format("%.0f", game.clock.currentHour) .. ":" .. string.format("%02.0f", game.clock.currentMin), 10, 10)
+	love.graphics.print(string.format("%.0f", game.clock.currentHour) .. ":" .. string.format("%02.0f", game.clock.currentMin) .. " " .. game.clock.amOrPm, 10, 10)
 end
 
 return registerClock
