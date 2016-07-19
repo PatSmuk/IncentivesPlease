6cdd499330ae9abba05110cc14ebe89105fd304e
Brian Olynyk <bolynyk@360insights.com>
2016-07-19 10:12:58 -0400

    fixing bug in counting strikes

--- systems/claimChecking.lua
+++ systems/claimChecking.lua
@@ -31,10 +31,29 @@ function CC.register(game)
   game:on("RENDER_UI", CC.renderClaimUI)
 end
 
+---------------
+-- DAY LOGIC --
+---------------
+
+function CC.startDay(game, message)
+  print("Starting day: " .. message.day)
+  game.claimChecking.dayEnded = false
+  game.claimChecking.dayStarted = true
+  game.claimChecking.currentDay = message.day
+  game.claimChecking.strikes = 0
+  game.claimChecking.claimsApproved = 0
+  game.claimChecking.claimsDenied = 0
+  game.claimChecking.dayBalance = 0
+end
+
 function CC.endDay(game, message)
   game.claimChecking.dayStarted = false
   game.claimChecking.dayEnded = true
 end
+
+-----------------
+-- CLAIM LOGIC --
+-----------------
 
 function CC.incrementClaimsApproved(game, message)
   local approvedClaim = message.claim
@@ -45,8 +64,12 @@ function CC.incrementClaimsApproved(game, message)
     game.claimChecking.totalBalance = game.claimChecking.totalBalance + SUCCESSFUL_ID_AMMOUNT
   else
     print("You claim identification is bad, and you should feel bad")
-    game.claimChecking.dayBalance = game.claimChecking.dayBalance - FAILED_ID_AMOUNT
-    game.claimChecking.totalBalance = game.claimChecking.totalBalance + FAILED_ID_AMOUNT
+    if game.claimChecking.claimsDenied < MAX_STRIKES then
+      game.claimChecking.strikes = game.claimChecking.strikes + 1
+    else
+      game.claimChecking.dayBalance = game.claimChecking.dayBalance - FAILED_ID_AMOUNT
+      game.claimChecking.totalBalance = game.claimChecking.totalBalance - FAILED_ID_AMOUNT
+    end
   end
 end
 
@@ -62,22 +85,15 @@ function CC.incrementClaimsDenied(game, message)
     if game.claimChecking.strikes < MAX_STRIKES then
       game.claimChecking.strikes = game.claimChecking.strikes + 1
     else
-      game.claimChecking.dayBalance = game.claimChecking.dayBalance + FAILED_ID_AMMOUNT
-      game.claimChecking.totalBalance = game.claimChecking.totalBalance + FAILED_ID_AMMOUNT
+      game.claimChecking.dayBalance = game.claimChecking.dayBalance - FAILED_ID_AMMOUNT
+      game.claimChecking.totalBalance = game.claimChecking.totalBalance - FAILED_ID_AMMOUNT
     end
   end
 end
 
-function CC.startDay(game, message)
-  print("Starting day: " .. message.day)
-  game.claimChecking.dayEnded = false
-  game.claimChecking.dayStarted = true
-  game.claimChecking.currentDay = message.day
-  game.claimChecking.strikes = 0
-  game.claimChecking.claimsApproved = 0
-  game.claimChecking.claimsDenied = 0
-  game.claimChecking.dayBalance = 0
-end
+------------------------------
+-- SHOW STUFF ON THE SCREEN --
+------------------------------
 
 function CC.renderClaimCounters(game, message)
   approvedText = game.claimChecking.claimsApproved
@@ -97,7 +113,6 @@ function CC.renderDayEnd(game, message)
 
   love.graphics.print(moneyMade, DAY_END_MESSAGE_X, 400)
   love.graphics.print(strikesText, DAY_END_MESSAGE_X, 450)
-
 end
 
 function CC.renderClaimUI(game, message)
